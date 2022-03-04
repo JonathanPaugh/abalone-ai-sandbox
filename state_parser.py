2e010a3b0c62eb2d6b12ef2f4238a6431ae63ad6
jeff <jeffphan99@gmail.com>
2022-03-04 09:40:24 -0800

    chi en notation->2d board function added

--- state_parser.py
+++ state_parser.py
@@ -1,3 +1,42 @@
+def translate_to_board(string):
+    pass
+
+
+def get_board():
+    return [[0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0, 0],
+            [0, 0, 0, 0, 0]]
+
+
+def translate_text_to_board(string):
+    mapping = {
+        "A": 8,
+        "B": 7,
+        "C": 6,
+        "D": 5,
+        "E": 4,
+        "F": 3,
+        "G": 2,
+        "H": 1,
+        "I": 0,
+    }
+    if ord(string[0]) > ord('E'):
+        position = int(string[1]) - int(ord(string[0]) - ord('E'))
+    else:
+        position = int(string[1])
+    if string[2] == "b":
+        colour = 2
+    else:
+        colour = 1
+    return [mapping[string[0]], position, colour]
+
+
 class StateParser:
     @staticmethod
     def convert_board_to_text(board) -> str:
@@ -27,7 +66,7 @@ class StateParser:
         return text
 
     @staticmethod
-    def convert_text_to_state(text) -> tuple[[], int]:
+    def convert_text_to_state(filename) -> tuple[[], int]:
         """
         Convert the text in format (including turn) b=player 1, w=player 2:
 
@@ -49,7 +88,14 @@ class StateParser:
         ], 1)
         """
 
-        board = []
-        turn = 0
+        board = get_board()
+        with open(filename) as file:
+            data = file.read().rstrip()
+            turn_color = (data[0])
+            untranslated_list = data[2:].split(",")
+            for string in untranslated_list:
+                translated = translate_text_to_board(string)
+                board[translated[0]][translated[1] - 1] = translated[2]
 
-        return board, turn
+            print(board)
+        return board, int(turn_color)
