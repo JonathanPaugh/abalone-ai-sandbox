5f8ef6e2f34590db0db88ab9e8530152c9b62df4
Angela Qi <angela.qity@gmail.com>
2022-03-04 12:21:24 -0800

    Added translate_board_to_text method

--- state_parser.py
+++ state_parser.py
@@ -4,31 +4,43 @@ from core.board import Board
 def translate_to_board(string):
     pass
 
+mapping = {
+    "A": 8,
+    "B": 7,
+    "C": 6,
+    "D": 5,
+    "E": 4,
+    "F": 3,
+    "G": 2,
+    "H": 1,
+    "I": 0,
+}
+
 
 def get_board():
-    return [[0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 0]]
+    return [
+             [0, 0, 0, 0, 0],
+             [2, 2, 0, 0, 1, 1],
+             [2, 2, 2, 0, 1, 1, 1],
+             [0, 2, 2, 0, 0, 1, 1, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 1, 0, 0, 2, 2, 0],
+             [1, 1, 1, 0, 2, 2, 2],
+             [1, 1, 0, 0, 2, 2],
+             [0, 0, 0, 0, 0]
+        ]
+    # return [[0, 0, 0, 0, 0],
+    #         [0, 0, 0, 0, 0, 0],
+    #         [0, 0, 0, 0, 0, 0, 0],
+    #         [0, 0, 0, 0, 0, 0, 0, 0],
+    #         [0, 0, 0, 0, 0, 0, 0, 0, 0],
+    #         [0, 0, 0, 0, 0, 0, 0, 0],
+    #         [0, 0, 0, 0, 0, 0, 0],
+    #         [0, 0, 0, 0, 0, 0],
+    #         [0, 0, 0, 0, 0]]
 
 
 def translate_text_to_board(string):
-    mapping = {
-        "A": 8,
-        "B": 7,
-        "C": 6,
-        "D": 5,
-        "E": 4,
-        "F": 3,
-        "G": 2,
-        "H": 1,
-        "I": 0,
-    }
     if ord(string[0]) > ord('E'):
         position = int(string[1]) - int(ord(string[0]) - ord('E'))
     else:
@@ -39,9 +51,26 @@ def translate_text_to_board(string):
         colour = 1
     return [mapping[string[0]], position, colour]
 
+
+def translate_board_to_text(board: Board) -> str:
+    string_positions = ""
+    current_state = Board.enumerate(board)
+    print(current_state)
+    for marbles, colour in current_state:
+        if colour is not None:
+            if colour == 1:
+                player = "b"
+            else:
+                player = "w"
+            string_positions += marbles + player + ", "
+    print(string_positions)
+    return string_positions
+
+
 class StateParser:
     @staticmethod
-    def convert_board_to_text(board: Board) -> str:
+    def convert_board_to_text(filename, board: Board):
+
         """
         convert the board ex:
 
@@ -63,9 +92,8 @@ class StateParser:
 
         """
 
-        text = ""
-
-        return text
+        with open(filename, mode="w") as f:
+            f.write(translate_board_to_text(board))
 
     @staticmethod
     def convert_text_to_state(filename) -> tuple[[], int]:
@@ -106,6 +134,7 @@ class StateParser:
             print(board)
         return board, int(turn_color)
 
+
     def convert_move_to_text(self, move) -> str:
         """
         Convert the move object to a move notation:
@@ -116,3 +145,31 @@ class StateParser:
 
         text = ""
         return text
+
+
+if __name__ == "__main__":
+    parser = StateParser
+    board = Board(get_board())
+    parser.convert_board_to_text("Test1.board", board)
+
+
+
+    #
+    # test = Board.enumerate(board)
+    # print(test)
+    # row = 0
+    # list_of_tuples = [(Hex(x=4, y=0), None), (Hex(x=5, y=0), None)]
+    # for position, colour in list_of_tuples:
+    #     x = list_of_tuples[row][0].get_x
+    #     y = list_of_tuples[row][0].get_y
+    #     if colour == 1:
+    #         text_colour = 2
+    #     elif colour == 2:
+    #         text_colour = 2
+    #     else:
+    #         text_colour = 0
+    #     row += 1
+    #     print(x, y, text_colour)
+    #
+    # print(list_of_tuples[0][0])
+
