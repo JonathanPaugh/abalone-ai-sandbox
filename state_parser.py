e9810883e638e9fc0b8064589eb1f41a7fbd5b26
Angela Qi <angela.qity@gmail.com>
2022-03-04 22:33:36 -0800

    Finished translate_board_to_text method. Added docstrings + reformatted.

--- state_parser.py
+++ state_parser.py
@@ -1,85 +1,46 @@
+"""This module contains methods to read parse input/output files containing current board states."""
+
 from core.board import Board
-
-def translate_to_board(string):
-    pass
-
-mapping = {
-    "A": 8,
-    "B": 7,
-    "C": 6,
-    "D": 5,
-    "E": 4,
-    "F": 3,
-    "G": 2,
-    "H": 1,
-    "I": 0,
-}
-
-def get_board():
-    return [
-             [0, 0, 0, 0, 0],
-             [2, 2, 0, 0, 1, 1],
-             [2, 2, 2, 0, 1, 1, 1],
-             [0, 2, 2, 0, 0, 1, 1, 0],
-             [0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 1, 1, 0, 0, 2, 2, 0],
-             [1, 1, 1, 0, 2, 2, 2],
-             [1, 1, 0, 0, 2, 2],
-             [0, 0, 0, 0, 0]
-        ]
-
-def translate_text_to_board(string):
-    if ord(string[0]) > ord('E'):
-        position = int(string[1]) - int(ord(string[0]) - ord('E'))
-    else:
-        position = int(string[1])
-    if string[2] == "b":
-        colour = 2
-    else:
-        colour = 1
-    return [mapping[string[0]], position, colour]
-
-
-def translate_board_to_text(board: Board) -> str:
-    string_positions = ""
-    current_state = Board.enumerate(board)
-    print(current_state)
-    for marbles, colour in current_state:
-        if colour is not None:
-            if colour == 1:
-                player = "b"
-            else:
-                player = "w"
-            string_positions += str(marbles) + player + ", "
-    print(string_positions)
-    return string_positions
+from core.color import Color
 
 
 class StateParser:
-    @staticmethod
-    def convert_board_to_text(filename, board: Board):
-        """
-        convert the board ex:
+    # Possibly to keep track of the Test<#>.board later? As per instructions.
+    # output_board_num = 1
+    #
+    # @classmethod
+    # def increase_text_output_num(cls):
+    #     cls.output_board_num += 1
+
+    empty_board_layout = \
         [
              [0, 0, 0, 0, 0],
-             [2, 2, 0, 0, 1, 1],
-             [2, 2, 2, 0, 1, 1, 1],
-             [0, 2, 2, 0, 0, 1, 1, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
              [0, 0, 0, 0, 0, 0, 0, 0, 0],
-             [0, 1, 1, 0, 0, 2, 2, 0],
-             [1, 1, 1, 0, 2, 2, 2],
-             [1, 1, 0, 0, 2, 2],
-             [0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0, 0],
+             [0, 0, 0, 0, 0]
         ]
-        to the text format (not including turn):
-        C5b,D5b,E4b,E5b,E6b,F5b,F6b,F7b,F8b,G6b,H6b,C3w,C4w,D3w,D4w,D6w,E7w,F4w,G5w,G7w,G8w,G9w,H7w,H8w,H9w
-        """
 
-        with open(filename, mode="w") as f:
-            f.write(translate_board_to_text(board))
+    # Keep for testing translate_board_to_text.
+    test_output_board_layout = \
+        [
+               [0, 0, 0, 0, 0],
+               [2, 2, 0, 0, 1, 1],
+               [2, 2, 2, 0, 1, 1, 1],
+               [0, 2, 2, 0, 0, 1, 1, 0],
+               [0, 0, 0, 0, 0, 0, 0, 0, 0],
+               [0, 1, 1, 0, 0, 2, 2, 0],
+               [1, 1, 1, 0, 2, 2, 2],
+               [1, 1, 0, 0, 2, 2],
+               [0, 0, 0, 0, 0]
+        ]
 
-    @staticmethod
-    def convert_text_to_state(filename) -> tuple[[], int]:
+    @classmethod
+    def convert_text_to_state(cls, filename) -> tuple[[], int]:
         """
         Convert the text in format (including turn) b=player 1, w=player 2:
         b
@@ -98,7 +59,7 @@ class StateParser:
         ], 1)
         """
 
-        board = get_board()
+        board = cls.empty_board_layout
         with open(filename) as file:
             data = file.read().rstrip()
             turn_color = (data[0])
@@ -114,7 +75,30 @@ class StateParser:
             print(board)
         return board, int(turn_color)
 
+    @staticmethod
+    def convert_board_to_text(filename, board: Board):
+        """
+        convert the board ex:
+        [
+             [0, 0, 0, 0, 0],
+             [2, 2, 0, 0, 1, 1],
+             [2, 2, 2, 0, 1, 1, 1],
+             [0, 2, 2, 0, 0, 1, 1, 0],
+             [0, 0, 0, 0, 0, 0, 0, 0, 0],
+             [0, 1, 1, 0, 0, 2, 2, 0],
+             [1, 1, 1, 0, 2, 2, 2],
+             [1, 1, 0, 0, 2, 2],
+             [0, 0, 0, 0, 0],
+        ]
+        to the text format (not including turn):
+        C5b,D5b,E4b,E5b,E6b,F5b,F6b,F7b,F8b,G6b,H6b,C3w,C4w,D3w,D4w,D6w,E7w,F4w,G5w,G7w,G8w,G9w,H7w,H8w,H9w and
+        writes to a Test<#>.board plaintext file.
+        """
 
+        with open(filename, mode="w") as f:
+            f.write(translate_board_to_text(board))
+
+    @staticmethod
     def convert_move_to_text(self, move) -> str:
         """
         Convert the move object to a move notation:
@@ -126,7 +110,53 @@ class StateParser:
         return text
 
 
+def translate_text_to_board(string):
+    mapping = {
+        "A": 8,
+        "B": 7,
+        "C": 6,
+        "D": 5,
+        "E": 4,
+        "F": 3,
+        "G": 2,
+        "H": 1,
+        "I": 0,
+    }
+
+    if ord(string[0]) > ord('E'):
+        position = int(string[1]) - int(ord(string[0]) - ord('E'))
+    else:
+        position = int(string[1])
+    if string[2] == "b":
+        colour = 2
+    else:
+        colour = 1
+    return [mapping[string[0]], position, colour]
+
+
+def translate_board_to_text(board: Board) -> str:
+    """Given a board representation, return a string containing all the occupied cells."""
+    tempt_positions = ""
+
+    current_state = Board.enumerate(board)
+    for marbles, colour in current_state:
+        if colour is not None:
+            if colour == Color.BLACK:
+                player = "b"
+            else:
+                player = "w"
+            tempt_positions += str(marbles) + player + ", "
+    positions_list = tempt_positions.split(" ")
+    positions_list.sort()
+    string_positions = "".join(map(str, positions_list))
+    #For testing.
+    print(string_positions[:(len(string_positions) - 1)])
+    return string_positions[:(len(string_positions) - 1)]
+
+
 if __name__ == "__main__":
     parser = StateParser
-    board = Board().create_from_data(get_board())
-    parser.convert_board_to_text("Test1.board", board)
+    board_layout = Board().create_from_data(parser.test_output_board_layout)
+    parser.convert_board_to_text("Test1.board", board_layout)
+
+    parser.convert_text_to_state("Test1.input")
