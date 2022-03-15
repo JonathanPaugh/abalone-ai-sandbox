521f44c9025a9b735f64ba530c2f6be81faeb00b
Jonathan Paugh <j.d.paugh@gmail.com>
2022-03-15 13:53:59 -0700

    Add annotations to find_next_move() for Agent

--- agent/agent.py
+++ agent/agent.py
@@ -1,11 +1,13 @@
 import random
 
 from agent.state_generator import StateGenerator
+from core.board import Board
+from core.color import Color
 from core.move import Move
 
 
 class Agent:
-    def find_next_move(self, board, player) -> Move:
+    def find_next_move(self, board: Board, player: Color) -> Move:
         moves = StateGenerator.enumerate_board(board, player)
         boards = StateGenerator.generate(board, moves) # TODO: Look at board outcomes, get heuristic values from each and return the move that lead to best outcome
         return random.choice(moves)
