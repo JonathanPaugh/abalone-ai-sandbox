1e7302961bd29b8b84d587042b95327d5049ea8c
Angela Qi <angela.qity@gmail.com>
2022-03-22 09:28:43 -0700

    Working on agent.

--- agent/agent.py
+++ agent/agent.py
@@ -3,6 +3,8 @@ import random
 from agent.state_generator import StateGenerator
 from core.board import Board
 from core.color import Color
+from core.constants import BOARD_SIZE
+from core.hex import Hex
 from core.move import Move
 
 
@@ -12,11 +14,10 @@ class Agent:
     def find_next_move(self, board: Board, player: Color) -> Move:
         moves = StateGenerator.enumerate_board(board, player)
         boards = StateGenerator.generate(board, moves) # TODO: Look at board outcomes, get heuristic values from each and return the move that lead to best outcome
-        #return random.choice(moves)
+        return random.choice(moves)
 
-        #E5 is center of board
-        list_of_marble_positions = []
-        for b in boards:
-            list_of_marble_positions.append(b.enumerate())
 
-            for position in list_of_marble_positions:
+
+
+
+
