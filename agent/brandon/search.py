2325893657de099b1fd8dbabad97bf0cfe8fc370
Brandon Semilla <semibran@gmail.com>
2022-04-01 00:30:51 -0700

    add negamax top-level move ordering

--- agent/brandon/search.py
+++ agent/brandon/search.py
@@ -13,6 +13,16 @@ class Search:
     An interface around Abalone search logic.
     """
 
+    @staticmethod
+    def _estimate_move_score(board, move):
+        WEIGHT_SUMITO = 10 # consider sumitos first
+        return (len(move.get_cells())
+            + WEIGHT_SUMITO * move.is_sumito(board))
+
+    @classmethod
+    def _order_moves(cls, board, moves):
+        return sorted(moves, key=lambda move: cls._estimate_move_score(board, move), reverse=True)
+
     def __init__(self):
         self.heuristic = None
         self._stopped = False
@@ -43,6 +53,7 @@ class Search:
 
     def _search(self, board: Board, color: Color, on_find: callable):
         moves = StateGenerator.enumerate_board(board, color)
+        moves = self._order_moves(board, moves)
         alpha = -inf
 
         for move in moves:
